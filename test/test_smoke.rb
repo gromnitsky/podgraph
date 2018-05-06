6d54363b6cf0895d9e982fa6d928eb3f6e84ce5e
gromnitsky <alexander.gromnitsky@gmail.com>
2018-05-06 21:26:15 +0300

    tests

--- test/test_smoke.rb
+++ test/test_smoke.rb
@@ -13,5 +13,33 @@ class TestTransformer < Minitest::Test
     tr = Transformer.new File.read "#{__dir__}/pre.html"
     assert_equal "tesing pre newlines", tr.subject
     assert_equal "tesing pre newlines", tr.subject # memoization
+
+    tr = Transformer.new '<p>1</p>'
+    assert_equal "no subject", tr.subject
+  end
+
+  def test_pre
+    skip
+    tr = Transformer.new "<!doctype html><pre><code>  1\n\n  2</code></pre>"
+    assert_equal "<!DOCTYPE html>\n<html><body><pre><code>&nbsp;1&nbsp;<br><br>&nbsp;&nbsp;2</code></pre></body></html>\n", tr.html
+  end
+
+  def test_svg
+    tr = Transformer.new "<!doctype html><p>1<img src='#{__dir__}/circle.svg'></p>"
+    assert_equal "<!DOCTYPE html>
+<html><body><p>1<img src=\"data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIj8+CjxzdmcgdmVyc2lvbj0iMS4xIiB3aWR0aD0iMzAwIiBoZWlnaHQ9IjIwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8Y2lyY2xlIGN4PSIxMDAiIGN5PSIxMDAiIHI9IjgwIiBmaWxsPSJibHVlIiAvPgogIDx0ZXh0IHg9IjEwMCIgeT0iMTI1IiBmb250LXNpemU9IjYwIiB0ZXh0LWFuY2hvcj0ibWlkZGxlIiBmaWxsPSJ5ZWxsb3ciPlNWRzwvdGV4dD4KPC9zdmc+Cg==\"></p></body></html>
+", tr.html
+  end
+
+  def test_raster_images
+    tr = Transformer.new "<!doctype html><img src='#{__dir__}/red.gif'><img src='#{__dir__}/red.gif"
+    assert_equal "<!DOCTYPE html>
+<html><body>
+<img src=\"cid:642a8e0e068af068d844962c2e0f9af7c73b798e\"><img src=\"cid:642a8e0e068af068d844962c2e0f9af7c73b798e\">
+</body></html>
+", tr.html
+    assert_equal({"642a8e0e068af068d844962c2e0f9af7c73b798e" => {
+                    :name => "#{__dir__}/red.gif"
+                  }}, tr.images)
   end
 end
